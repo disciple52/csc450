08629c8a6df23fe5271e7196e81b2b2338fe0309
disciple52 <ameier565@gmail.com>
2016-02-19 17:00:34 +0000

    second commit

--- copyReverse.c
+++ copyReverse.c
@@ -2,36 +2,55 @@
 #include <fcntl.h>
 #include "tlpi_hdr.h"
 #ifndef BUF_SIZE
-#define BUF_SIZE 1024
+#define BUF_SIZE 1
 #endif
 /* Allow "cc -D" to override definition */
- 
-void main(int argc, char *argv[])
+
+
+int main(int argc, char *argv[])
 {
-    int cnt;
-    char ch, ch1;
-    FILE *fp1, *fp2;
- 
-    if (fp1 = fopen(argv[1], "r"))    
-    {
-        fp2 = fopen(argv[2], "w");
-        cnt = count_characters(fp1); 
-        fseek(fp1, -1L, 2);
- 
-        while (cnt)
-        {
-            ch = fgetc(fp1);
-            fputc(ch, fp2);
-            fseek(fp1, -2L, 1);
-            cnt--;
-        }
-    }
+  int numBytes = getNumBytes(argv[1]);
+  int outputFd, openFlags; mode_t filePerms;
+  openFlags = O_CREAT | O_WRONLY | O_TRUNC;
+  filePerms = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH; /* rw-rw-rw- */
+  outputFd = open(argv[2], openFlags, filePerms);
+  int i = numBytes;
+  char answer[BUF_SIZE];
+  
+  for(; i > 0; i--)
+  {
+    answer[0] = getCharAtIndex(i, argv[1]);
+    write(outputFd, answer, BUF_SIZE);
+    //printf("%c", getCharAtIndex(i, argv[1]));
+  }
+  close(outputFd);
+  exit(EXIT_SUCCESS);
 }
 
-int count_characters(FILE *f) 
+int getCharAtIndex(int index, char* fn)
 {
-    fseek(f, -1L, 2);
-    int last_pos = ftell(f);
-    last_pos++;
-    return last_pos;
+  int inputFd = open(fn, O_RDONLY);
+  ssize_t numRead;
+  char buf[BUF_SIZE];
+  int i = 0;
+  for(; i < index; i++)
+  {
+    read(inputFd, buf, BUF_SIZE);
+  }
+  close(inputFd);
+  return buf[0]; //buf
+}
+
+int getNumBytes(char* fn)
+{
+  int inputFd = open(fn, O_RDONLY);
+  ssize_t numRead;
+  int numBytes = 0;
+  char buf[BUF_SIZE];
+  while ((numRead = read(inputFd, buf, BUF_SIZE)) > 0)
+  {
+    numBytes++;
+  }
+  close(inputFd);
+  return numBytes;
 }
